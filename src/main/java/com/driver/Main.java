71844ee0f6627d4347e787977154f776bd2559cb
Rushikesh <rushikeshkalbande026@gmail.com>
2023-04-20 20:34:32 +0530

    Second Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,19 @@ public class Main {
         B.meth();
     }
 }
+ class A {
+    // B obj = new B();
+    // B.meth();
+    public static void meth(){
+        System.out.println("Invoking method from class A");
+
+    }
+}
+class B extends A{
+    // B obj = new B();
+
+    public static void meth(){
+        System.out.println("Method is overridden in Extendend class B");
+    }
+
+}
